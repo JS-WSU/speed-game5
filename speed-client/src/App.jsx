9c4dc3f52f8c50d062f5414c24a5979aae1ab6eb
Justin Singh <justinsingh@mail.weber.edu>
2023-07-10 16:15:29 -0600

    Added routes for React

--- speed-client/src/App.jsx
+++ speed-client/src/App.jsx
@@ -1,33 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import HelloWorld from "./components/HelloWorld";
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Lobby from "./components/Lobby";
 import axios from "axios";
+import "./App.css";
 
 function App() {
   axios.defaults.withCredentials = true;
-  const [record, setRecord] = useState(null);
-  async function getJohn() {
-    try {
-      let { data } = await axios.get("http://localhost:4000/records/john");
-      setRecord(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
+  const [userSession, setUserSession] = useState(null);
+
+  useEffect(() => {
+    const fetchUserAuth = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:4000/users/isLoggedIn`
+        );
+        setUserSession(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserAuth();
+  }, []);
+
   return (
-    <div>
-      <div>Hello World!</div>
-      {record &&
-        Object.keys(record[0]).map((key, index) => {
-          console.log(record)
-          return (
-            <div key={index}>
-              <div>
-                {key}: {record[0][key]}
-              </div>
-            </div>
-          );
-        })}
-      <button onClick={getJohn}>Click Me!</button>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<HelloWorld />} />
+        <Route
+          path="/register"
+          element={<Register userSession={userSession} />}
+        />
+        <Route path="/login" element={<Login userSession={userSession} />} />
+        <Route element={<ProtectedRoute userSession={userSession} />}>
+          <Route path="/lobby" element={<Lobby />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
