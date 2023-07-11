7e7691ae6d75a3acbeff8213e103cc3d97e0a004
Justin Singh <justinsingh@mail.weber.edu>
2023-07-11 17:34:37 -0600

    HelloWorld Page

---
+++ speed-client/src/pages/HelloWorld.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function HelloWorld() {
+  const [record, setRecord] = useState(null);
+  const getJohn = async () => {
+    try {
+      let { data } = await axios.get("/records/john");
+      setRecord(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <main className="container">
+      <div>
+        <h1>Hello World!</h1>
+        <button className="btn btn-primary" onClick={getJohn}>
+          Click Me!
+        </button>
+      </div>
+      {record &&
+        Object.keys(record).map((key, index) => {
+          console.log(record);
+          return (
+            <div key={index}>
+              <div>
+                {key}: {record[key]}
+              </div>
+            </div>
+          );
+        })}
+    </main>
+  );
+}
