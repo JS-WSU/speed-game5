619f66783d927a9c78cb370c08303f9817ccb52c
Justin Singh <justinsingh@mail.weber.edu>
2023-07-11 17:32:16 -0600

    Mongoose Connection instead of MongoClient

--- speed-server/db/conn.mjs
+++ speed-server/db/conn.mjs
@@ -1,25 +1,25 @@
-import { MongoClient } from "mongodb";
-// import mongoose from "mongoose"
+// import { MongoClient } from "mongodb";
+import mongoose from "mongoose"
 
 const connectionString = process.env.ATLAS_URI || "";
 
-// try {
-//   await mongoose.connect(connectionString, {dbName: "sample_training"})
-//   console.log("Succesfully connected to MongoDB cluster using Mongoose")
-// } catch(e) {
-//   console.error(e);
-// }
-
-const client = new MongoClient(connectionString);
-
-let conn;
 try {
-  conn = await client.connect();
-  console.log("Succesfully connected to MongoDB cluster")
+  await mongoose.connect(connectionString, {dbName: "sample_training"})
+  console.log("Succesfully connected to MongoDB cluster using Mongoose")
 } catch(e) {
   console.error(e);
 }
 
-let db = conn.db("sample_training");
+// const client = new MongoClient(connectionString);
 
-export default db;
+// let conn;
+// try {
+//   conn = await client.connect();
+//   console.log("Succesfully connected to MongoDB cluster")
+// } catch(e) {
+//   console.error(e);
+// }
+
+// let db = conn.db("sample_training");
+
+// export default db;
