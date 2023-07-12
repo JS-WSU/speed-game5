ff966bb27298f208055895e60e49520c77544b93
Justin Singh <justinsingh@mail.weber.edu>
2023-07-12 05:58:03 -0600

    Basic html headings for Home Page

--- speed-client/src/pages/Home.jsx
+++ speed-client/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <main className="container">Home</main>;
+  return (
+    <main className="container">
+      <h1>Welcome to Speed!</h1>
+      <div>
+        <h2>Instructions</h2>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
