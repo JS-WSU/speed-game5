de01e0184787ddd5e779f718a38df5d5c3fa85ac
Justin Singh <justinsingh@mail.weber.edu>
2023-07-11 21:18:53 -0600

    Unnecessary old files

--- speed-client/src/components/Lobby.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Lobby() {
-  return <div>This is the lobby</div>;
-}
--- speed-client/src/components/Register.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function Register({ userSession }) {
-  return userSession ? <Navigate to="/lobby"></Navigate> : <div>Register</div>;
-}
