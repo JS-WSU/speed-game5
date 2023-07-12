69f5cf5eb019fa7e40894744177469d5a2fb951f
Justin Singh <justinsingh@mail.weber.edu>
2023-07-11 21:15:28 -0600

    Alert component to see within whole app

---
+++ speed-client/src/components/Alert.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useContext } from "react";
+import AlertContext from "../context/AlertContext";
+
+const Alert = () => {
+  const { alert, clear } = useContext(AlertContext);
+
+  return alert && alert.status === "success" ? (
+    <button
+      className={`w-100 rounded-0 alert alert-success d-flex justify-content-center`}
+      onClick={clear}
+    >
+      <i className="bi bi-check2-circle me-2 fs-4 text-success"></i>
+      <span className="align-self-center">{alert.message}</span>
+    </button>
+  ) : alert ? (
+    <button
+      className={`w-100 rounded-0 alert alert-danger d-flex justify-content-center`}
+      onClick={clear}
+    >
+      <i className="bi bi-exclamation-octagon me-2 fs-4 text-danger"></i>
+      <span className="align-self-center">{alert.message}</span>
+    </button>
+  ) : (
+    <></>
+  );
+};
+
+export default Alert;
