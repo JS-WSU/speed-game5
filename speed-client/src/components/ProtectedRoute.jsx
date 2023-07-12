4a1c4d2d2cfea0ed9f92a74f0f3ed8aad50a2121
Justin Singh <justinsingh@mail.weber.edu>
2023-07-11 21:14:12 -0600

    Adding localstorage to deal with page refresh

--- speed-client/src/components/ProtectedRoute.jsx
+++ speed-client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-export default function ProtectedRoute({ userSession }) {
+export default function ProtectedRoute() {
+  const userSession = localStorage.getItem("userSession");
+
   return !userSession ? <Navigate to="/" replace /> : <Outlet />;
 }
