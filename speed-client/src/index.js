f406daf626c14e3fd5ee636d63afb20f5b57034d
Justin Singh <justinsingh@mail.weber.edu>
2023-07-11 21:15:41 -0600

    Getting rid of unneeded imports

--- speed-client/src/index.js
+++ speed-client/src/index.js
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom/client';
-import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import { AlertProvider } from "./context/AlertContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <AlertProvider>
+      <App />
+    </AlertProvider>
   </BrowserRouter>
 );
 
