e449fac0f009f259c74c831fcc31a559c2fcafc6
Justin Singh <justinsingh@mail.weber.edu>
2023-07-12 05:56:58 -0600

    Getting rid of MongoClient comments

--- speed-server/db/conn.mjs
+++ speed-server/db/conn.mjs
@@ -1,25 +1,10 @@
-// import { MongoClient } from "mongodb";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectionString = process.env.ATLAS_URI || "";
 
 try {
-  await mongoose.connect(connectionString, {dbName: "sample_training"})
-  console.log("Succesfully connected to MongoDB cluster using Mongoose")
-} catch(e) {
+  await mongoose.connect(connectionString, { dbName: "sample_training" });
+  console.log("Succesfully connected to MongoDB cluster using Mongoose");
+} catch (e) {
   console.error(e);
 }
-
-// const client = new MongoClient(connectionString);
-
-// let conn;
-// try {
-//   conn = await client.connect();
-//   console.log("Succesfully connected to MongoDB cluster")
-// } catch(e) {
-//   console.error(e);
-// }
-
-// let db = conn.db("sample_training");
-
-// export default db;
