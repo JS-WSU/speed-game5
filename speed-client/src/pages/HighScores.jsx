9cf9fd6665c6a23456893ce05fac44cb81ba285c
Justin Singh <justinsingh@mail.weber.edu>
2023-07-12 21:57:49 -0600

    Working on high scores ranking page

--- speed-client/src/pages/HighScores.jsx
+++ speed-client/src/pages/HighScores.jsx
@@ -3,36 +3,77 @@ import axios from "axios";
 
 function HighScores({ userSession }) {
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
 
-  useEffect(() => {});
+  const [onTable, setOnTable] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:4000/users/");
+        setUsers(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:4000/users/${userSession.username}`
+      );
+      setUser(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <main className="container">
-      <table class="table table-responsive table-hover">
-        <thead>
-          <tr>
-            <th scope="col">PLace</th>
-            <th scope="col">User</th>
-            <th scope="col">Win Percentage</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-        </tbody>
-      </table>
-      <div>
-        <h2>Your winrate: </h2>
-        <h2>Total Games Played:</h2>
-      </div>
+      {users.length ? (
+        <div className="m-auto">
+          <h1 className="text-center">High Scores Table</h1>
+          <table className="table table-responsive table-hover table-bordered">
+            <caption className="text-center">Top 10</caption>
+            <thead>
+              <tr>
+                <th scope="col">Place</th>
+                <th scope="col">User</th>
+                <th scope="col">Win Percentage</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr
+                  key={index}
+                  className={`${
+                    userSession.username === user.username ? "table-active" : ""
+                  }`}
+                >
+                  <th scope="row">{index + 1}</th>
+                  <td>{user.username}</td>
+                  <td>
+                    {Number(user.percentage).toLocaleString(undefined, {
+                      style: "percent",
+                      minimumFractionDigits: 2,
+                    })}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div>
+            <h2>Your winrate: </h2>
+            <h2>Total Games Played:</h2>
+          </div>
+        </div>
+      ) : (
+        <h1 className="text-center">No users currently</h1>
+      )}
     </main>
   );
 }
