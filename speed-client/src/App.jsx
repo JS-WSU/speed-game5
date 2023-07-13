cf2ee7fb97253055a03b0828fc93c4b5c44e6383
Justin Singh <justinsingh@mail.weber.edu>
2023-07-13 17:13:51 -0600

    using isAuth useState for render trigger

--- speed-client/src/App.jsx
+++ speed-client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
@@ -16,47 +16,48 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from "./components/Footer";
 import Alert from "./components/Alert";
 import Home from "./pages/Home";
+import GetErrorMessage from "./utils/GetErrorMessage.mjs";
 
 axios.defaults.withCredentials = true;
 
 function App() {
-  const [userSession, setUserSession] = useState(null);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserAuth = async () => {
+      // check if session exists
       try {
         const { data } = await axios.get(
           `http://localhost:4000/users/authenticated`
         );
-        setUserSession(data);
+        localStorage.setItem("userSession", JSON.stringify(data));
+        setIsAuth(true);
       } catch (error) {
-        console.log(error.message);
+        // if session expired, remove from local storage
+        if (localStorage.getItem("userSession")) {
+          localStorage.removeItem("userSession");
+          navigate("/login");
+        }
+        console.log(GetErrorMessage(error));
       }
     };
     fetchUserAuth();
-  }, []);
+  }, [navigate]);
 
   return (
     <>
-      <Navbar userSession={userSession} setUserSession={setUserSession} />
+      <Navbar isAuth={isAuth} setIsAuth={setIsAuth} />
       <Alert />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/hello-world" element={<HelloWorld />} />
-        <Route
-          path="/register"
-          element={<Register setUserSession={setUserSession} />}
-        />
-        <Route
-          path="/login"
-          element={<Login setUserSession={setUserSession} />}
-        />
+        <Route path="/register" element={<Register setIsAuth={setIsAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         <Route element={<ProtectedRoute />}>
-          <Route path="/lobby" element={<Lobby userSession={userSession} />} />
-          <Route
-            path="/high-scores"
-            element={<HighScores userSession={userSession} />}
-          />
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/high-scores" element={<HighScores />} />
         </Route>
         <Route path="*" element={<Error />} />
       </Routes>
