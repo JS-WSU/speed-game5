486b46302674f43edc398e525d1400441723159c
Justin Singh <justinsingh@mail.weber.edu>
2023-07-13 21:55:34 -0600

    updating high scores to also get current user

--- speed-client/src/pages/HighScores.jsx
+++ speed-client/src/pages/HighScores.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import GetErrorMessage from "../utils/GetErrorMessage.mjs";
 
 function HighScores() {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
+  const [users, setUsers] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const [onTable, setOnTable] = useState(false);
 
@@ -13,35 +15,43 @@ function HighScores() {
       try {
         const { data } = await axios.get("http://localhost:4000/users/");
         setUsers(data);
-        console.log(data);
+
+        let playerFound = data.find(
+          (player) =>
+            player.username ===
+            JSON.parse(localStorage.getItem("userSession")).username
+        );
+
+        if (playerFound) {
+          setOnTable(true);
+        }
+      } catch (error) {
+        console.log(GetErrorMessage(error));
+      }
+    };
+    const fetchUser = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:4000/users/user/${
+            JSON.parse(localStorage.getItem("userSession")).username
+          }`
+        );
+        setUser(data);
       } catch (error) {
         console.log(GetErrorMessage(error));
       }
     };
     fetchUsers();
+    fetchUser();
+    setLoading(false);
   }, []);
-
-  const fetchUser = async () => {
-    try {
-      const { data } = await axios.get(
-        `http://localhost:4000/users/${
-          JSON.parse(localStorage.getItem("userSession")).username
-        }`
-      );
-      setUser(data);
-      console.log(data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
 
   return (
     <main className="container">
-      {users.length ? (
+      {users ? (
         <div className="m-auto">
           <h1 className="text-center">High Scores Table</h1>
           <table className="table table-responsive table-bordered">
-            <caption className="text-center">Top 10</caption>
             <thead>
               <tr>
                 <th scope="col">Place</th>
@@ -51,23 +61,36 @@ function HighScores() {
             </thead>
             <tbody>
               {users.map((user, index) => {
-                if (
-                  user.username ===
-                  JSON.parse(localStorage.getItem("userSession")).username
-                ) {
-                  setOnTable(true);
+                if (user.totalGames > 0) {
+                  return (
+                    <tr
+                      key={index}
+                      className={`${
+                        JSON.parse(localStorage.getItem("userSession")) &&
+                        JSON.parse(localStorage.getItem("userSession"))
+                          .username === user.username
+                          ? "table-active"
+                          : ""
+                      }`}
+                    >
+                      <th scope="row">{index + 1}</th>
+                      <td>{user.username}</td>
+                      <td>
+                        {Number(user.percentage).toLocaleString(undefined, {
+                          style: "percent",
+                          minimumFractionDigits: 2,
+                        })}
+                      </td>
+                    </tr>
+                  );
+                } else {
+                  console.log(`${user.username} has not played any games`);
+                  return "";
                 }
-
-                <tr
-                  key={index}
-                  className={`${
-                    JSON.parse(localStorage.getItem("userSession")).username ===
-                    user.username
-                      ? "table-active"
-                      : ""
-                  }`}
-                >
-                  <th scope="row">{index + 1}</th>
+              })}
+              {!onTable && user && user.totalGames !== 0 && (
+                <tr className="table-active">
+                  <th scope="row">{user.rank}</th>
                   <td>{user.username}</td>
                   <td>
                     {Number(user.percentage).toLocaleString(undefined, {
@@ -75,17 +98,30 @@ function HighScores() {
                       minimumFractionDigits: 2,
                     })}
                   </td>
-                </tr>;
-              })}
+                </tr>
+              )}
             </tbody>
           </table>
-          <div>
-            <h2>Your winrate: </h2>
-            <h2>Total Games Played:</h2>
-          </div>
+
+          {user && user.totalGames !== 0 && (
+            <h4>Total Games Played: {user.totalGames}</h4>
+          )}
+          {user && user.totalGames === 0 && (
+            <div className="d-flex flex-column">
+              {" "}
+              <h4>You currently haven't played any games</h4>
+              <Link to="/lobby" className="btn btn-primary mx-auto">
+                Go To Lobby
+              </Link>
+            </div>
+          )}
         </div>
-      ) : (
+      ) : !loading ? (
         <h1 className="text-center">No users currently</h1>
+      ) : (
+        <h1 className="spinner-border m-auto">
+          <span className="visually-hidden">Loading...</span>
+        </h1>
       )}
     </main>
   );
