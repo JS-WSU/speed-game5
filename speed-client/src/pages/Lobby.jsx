965df7a01740c686504ed1987a1ad1a35f1aa144
Justin Singh <justinsingh@mail.weber.edu>
2023-07-13 21:55:04 -0600

    getting rid of unnecessary null check

--- speed-client/src/pages/Lobby.jsx
+++ speed-client/src/pages/Lobby.jsx
@@ -4,8 +4,7 @@ export default function Lobby() {
   return (
     <main className="container">
       This is the lobby
-      {JSON.parse(localStorage.getItem("userSession")) &&
-        JSON.parse(localStorage.getItem("userSession")).username}
+      {JSON.parse(localStorage.getItem("userSession")).username}
     </main>
   );
 }
