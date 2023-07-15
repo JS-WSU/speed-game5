a7296b687c35d474c87de722aec88deffa90b164
Justin Singh <justinsingh@mail.weber.edu>
2023-07-15 10:29:33 -0600

    adding popup, probably need component

--- speed-client/src/pages/Lobby.jsx
+++ speed-client/src/pages/Lobby.jsx
@@ -1,12 +1,53 @@
 import React from "react";
 
-export default function Lobby({ userSession}) {
+export default function Lobby({ userSession, setPopup, setGameInProcess }) {
+  const chooseGameType = () => {
+    setPopup(
+      <div className="modal d-block" tabIndex="-1">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header d-flex justify-content-center">
+              <h5 className="modal-title">Select Speed Type</h5>
+              <button
+                type="button"
+                class="btn-close"
+                onClick={() => setPopup(null)}
+              ></button>
+            </div>
+            <div className="modal-body text-center d-flex justify-content-between">
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={() => setPopup(null)}
+              >
+                California Speed
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => setPopup(null)}
+              >
+                Regular Speed
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <main className="container">
-      <h1> This is the lobby</h1>
-      <div>
-        User: {userSession && userSession.username}
+      <div className="d-flex justify-content-between">
+        <h1>Games</h1>
+        <button
+          className="btn btn-primary align-self-center"
+          onClick={chooseGameType}
+        >
+          Host Game
+        </button>
       </div>
+      <div>User: {userSession && userSession.username}</div>
     </main>
   );
 }
