2606d98815366a7e476c50d76e96f5efd79ae40a
Justin Singh <justinsingh@mail.weber.edu>
2023-07-17 18:21:53 -0600

    getting rid of unnecessary extra server

--- speed-server/server.mjs
+++ speed-server/server.mjs
@@ -15,55 +15,6 @@ const app = express();
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 
-const server =http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000'
-    }
-});
-
-io.of('/game').on('connection', (socket) => {
-    console.log("User connected: " + socket.id);
-    socket.emit('receive_message', JSON.stringify(
-      [
-        {
-          name: "Aleix Melon",
-          id: "E00245",
-          role: [
-            "Dev",
-            "DBA"
-          ],
-          age: 23,
-          doj: "11-12-2019",
-          married: false,
-          address: {
-            street: "32, Laham St.",
-            city: "Innsbruck",
-            country: "Austria"
-          },
-          referredby: "E0012"
-        },
-        {
-          name: "Bob Washington",
-          id: "E01245",
-          role: [
-            "HR"
-          ],
-          age: 43,
-          doj: "10-06-2010",
-          married: true,
-          address: {
-            street: "45, Abraham Lane.",
-            city: "Washington",
-            country: "USA"
-          },
-          referredby: null
-        }
-      ]
-    ));
-})
-
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -78,11 +29,52 @@ app.use(
 app.use("/records", records);
 app.use("/users", users);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-server.listen(4001, () => {
-  console.log(`Listening on port: 4001`);
+io.of("/game").on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+  socket.emit(
+    "receive_message",
+    JSON.stringify([
+      {
+        name: "Aleix Melon",
+        id: "E00245",
+        role: ["Dev", "DBA"],
+        age: 23,
+        doj: "11-12-2019",
+        married: false,
+        address: {
+          street: "32, Laham St.",
+          city: "Innsbruck",
+          country: "Austria",
+        },
+        referredby: "E0012",
+      },
+      {
+        name: "Bob Washington",
+        id: "E01245",
+        role: ["HR"],
+        age: 43,
+        doj: "10-06-2010",
+        married: true,
+        address: {
+          street: "45, Abraham Lane.",
+          city: "Washington",
+          country: "USA",
+        },
+        referredby: null,
+      },
+    ])
+  );
+});
+
+// start server
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
 });
