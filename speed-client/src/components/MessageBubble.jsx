fe1ac61da02a9aaae648fc7c4b7f53b4e21744c8
Justin Singh <justinsingh@mail.weber.edu>
2023-07-18 15:22:11 -0600

    Adding and using moment time package for mongodb date and also editing text bubbles

--- speed-client/src/components/MessageBubble.jsx
+++ speed-client/src/components/MessageBubble.jsx
@@ -1,55 +1,78 @@
 import React from "react";
+import moment from "moment";
+import { format } from "date-fns";
 
 function MessageBubble({ username, body, date }) {
-  const currentDate = new Date();
-  let dateDiff = currentDate - Date.parse(date);
-  var messageSentTime = Math.floor(dateDiff / 1000 / 60);
+  const DateTest = new Date(date);
 
-  if (messageSentTime > 1440)
-    messageSentTime =
-      Math.floor(messageSentTime / 60 / 24).toString() + " hours ago";
-  else if (messageSentTime > 60)
-    messageSentTime =
-      Math.floor(messageSentTime / 60).toString() + " minutes ago";
+  const convertedDate = moment(DateTest).fromNow();
+
+  // const currentDate = new Date();
+  // let dateDiff = currentDate - Date.parse(date);
+  // var messageSentTime = Math.floor(dateDiff / 1000 / 60);
+
+  // if (messageSentTime > 1440)
+  //   messageSentTime =
+  //     Math.floor(messageSentTime / 60 / 24).toString() + " hours ago";
+  // else if (messageSentTime > 60)
+  //   messageSentTime =
+  //     Math.floor(messageSentTime / 60).toString() + " minutes ago";
 
   return (
     <div className="mb-3 ms-1 d-flex flex-column p-3">
       <div className={`d-flex flex-row`}>
         <div
-          className={`card ${
-            username ===
-            JSON.parse(localStorage.getItem("userSession")).username
-              ? "ms-auto"
-              : ""
-          }`}
+          className={`d-flex flex-column
+        ${
+          username === JSON.parse(localStorage.getItem("userSession")).username
+            ? "ms-auto"
+            : ""
+        }
+        `}
         >
           <div
-            className={`card-body p-2 ${
+            className={`text-secondary  ${
               username ===
               JSON.parse(localStorage.getItem("userSession")).username
-                ? "bg-primary text-light rounded"
+                ? "align-self-end"
+                : ""
+            }
+        `}
+          >
+            {convertedDate}
+          </div>
+
+          <div
+            className={`card ${
+              username ===
+              JSON.parse(localStorage.getItem("userSession")).username
+                ? "ms-auto"
                 : ""
             }`}
           >
-            {body}
+            <div
+              className={`card-body p-2 ${
+                username ===
+                JSON.parse(localStorage.getItem("userSession")).username
+                  ? "bg-primary text-light rounded"
+                  : ""
+              }`}
+            >
+              {body}
+            </div>
           </div>
-        </div>
-        <div
-          className={`text-light h8
-        `}
-        >
-          {messageSentTime}
-        </div>
-      </div>
-      <div
-        className={`
+          <div
+            className={`
        ${
          username === JSON.parse(localStorage.getItem("userSession")).username
            ? "align-self-end"
            : ""
        }`}
-      >
-        {username}
+          >
+            {" "}
+            {username}
+          </div>
+        </div>
       </div>
     </div>
   );
