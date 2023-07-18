9a0142365de340ba6bca7b3dde2b9809030e0e01
Justin Singh <justinsingh@mail.weber.edu>
2023-07-18 09:00:00 -0600

    lobby useeffect socket conn

--- speed-client/src/pages/Lobby.jsx
+++ speed-client/src/pages/Lobby.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Popup from "reactjs-popup";
+import { io } from "socket.io-client";
+
+const socket = io.connect("http://localhost:4000/chat", { autoConnect: false });
 
 export default function Lobby({ userSession, setPopup, setGameInProcess }) {
   const [show, setShow] = useState(false);
+  const [users, setUsers] = useState([]);
 
   const chooseGameType = () => {
     setShow(true);
   };
 
+  useEffect(() => {
+    const GetUsers = (users) => {
+      setUsers(users);
+    };
+
+    socket.connect();
+
+    socket.on("chat_messages", GetUsers);
+
+    return () => socket.off("chat_messages", GetUsers);
+  }, []);
+
   return (
     <main className="container">
       <div className="d-flex justify-content-between">
-        <Popup open={show} className="popup-content" onClose={() => setShow(false)}>
+        <Popup
+          open={show}
+          className="popup-content"
+          onClose={() => setShow(false)}
+        >
           <div className="">
             <h5 className="text-center ">Select Speed Type</h5>
             <div className="d-flex justify-content-evenly">
@@ -44,6 +64,16 @@ export default function Lobby({ userSession, setPopup, setGameInProcess }) {
         </button>
       </div>
       <div>User: {userSession?.username}</div>
+      {users.length &&
+        users.map((user) => (
+          <div
+            className={`${
+              user.username === userSession?.username ? "bg-danger" : ""
+            }`}
+          >
+            {JSON.stringify(user)}
+          </div>
+        ))}
     </main>
   );
 }
