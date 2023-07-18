7fe5249282ac96c6c7e0332dd1ae1e263895111f
Justin Singh <justinsingh@mail.weber.edu>
2023-07-17 18:14:37 -0600

    messing around with game component

--- speed-client/src/pages/Game.jsx
+++ speed-client/src/pages/Game.jsx
@@ -1,32 +1,33 @@
-import io from 'socket.io-client';
-import { useEffect, useState } from 'react';
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 export default function Game() {
-    const [messageRecieved, setMessageRecieved] = useState([]);
+  const [messageRecieved, setMessageRecieved] = useState([]);
 
-    useEffect(() => {
-        const socket = io.connect('http://localhost:4001/game');
+  useEffect(() => {
+    const socket = io.connect("http://localhost:4001/game");
 
-        function messageReceivedHandler(data){
-            console.log(data);
-            const dataParsed = JSON.parse(data);
-            console.log(dataParsed);
-            setMessageRecieved(dataParsed);
-        }
-        socket.on('receive_message', messageReceivedHandler);
-        return () => {
-            socket.off('receive_message', messageReceivedHandler);
-        };
-    },[]);
-    
-    return (
-        <div>
-            <div>Message: {messageRecieved && messageRecieved.map((value, index) => {
-                return(
-                    <pre>{JSON.stringify(value)}</pre>
-                );
-            })}
-            </div>
-        </div>
-    );
+    function messageReceivedHandler(data) {
+      console.log(JSON.parse(data));
+      const dataParsed = JSON.parse(data);
+      console.log(dataParsed);
+      setMessageRecieved(dataParsed);
+    }
+    socket.on("receive_message", messageReceivedHandler);
+    return () => {
+      socket.off("receive_message", messageReceivedHandler);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>
+        Message:{" "}
+        {
+          messageRecieved.map((value, index) => {
+            return <pre>{JSON.stringify(value)}</pre>;
+          })}
+      </div>
+    </div>
+  );
 }
