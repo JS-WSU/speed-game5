44042eda246c1e1be78f6890da4af1a330063ccd
Justin Singh <justinsingh@mail.weber.edu>
2023-07-30 20:52:38 -0600

    get rid of draggable for now

--- speed-client/src/components/CardDraggable.jsx
+++ speed-client/src/components/CardDraggable.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useDrag } from "react-dnd";
-import Draggable from "react-draggable";
 
 function Card({ name, src, value }) {
   const [{ isDragging }, drag] = useDrag(() => ({
@@ -12,22 +11,20 @@ function Card({ name, src, value }) {
   }));
 
   return (
-    <Draggable>
-      <div
-        className={`d-flex flex-grow-1 me-2`}
+    <div
+      className={`d-flex flex-grow-1 me-2`}
+      style={{ maxWidth: "125px" }}
+      value={value}
+      ref={drag}
+    >
+      <img
+        draggable="false"
+        className="img-fluid"
+        src={src}
+        alt={name}
         style={{ maxWidth: "125px" }}
-        value={value}
-        ref={drag}
-      >
-        <img
-          draggable="false"
-          className="img-fluid"
-          src={src}
-          alt={name}
-          style={{ maxWidth: "125px" }}
-        />
-      </div>
-    </Draggable>
+      />
+    </div>
   );
 }
 
